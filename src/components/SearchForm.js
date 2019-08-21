572f72fab4b5efad1c79b722ab219a9bd8ffc490
axcs <aleix.cortines@gft.com>
2019-08-21 11:05:40 +0200

    solving link api

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -18,7 +18,7 @@ export class SearchForm extends Component{
             //Finally, We capture the value on the form
             e.preventDefault()
             const {inputMovie} = this.state;
-            fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+            fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
             .then(res =>res.json())
             .then(results => {
                 //extract on results the property search and total results
